8f5f5c55cd4cd7f4f5000fea439f301c01f3d319
Fiskerik <63838726+Fiskerik@users.noreply.github.com>
2023-08-15 13:32:20 +0000

    Test

---
+++ Assignment1/functionalprogramming.js
@@ -0,0 +1,24 @@
+// Task 1: Build a function-based console log message generator
+function consoleStyler() {
+    var message = "%c" + "hej";
+    var style = `color: ${color};`
+    style += `background: ${background};`
+    style += `font-size: ${fontSize};`
+    console.log(message);
+    alert("Hello friend!");
+    
+}
+
+// Task 2: Build another console log message generator
+function celebrateStyler() {
+    
+}
+
+// Task 3: Run both the consoleStyler and the celebrateStyler functions
+
+
+// Task 4: Insert a congratulatory and custom message
+function styleAndCelebrate() {
+    
+}
+// Call styleAndCelebrate
